779bcc827e3c24c848a8ec77406067d7c6964d21
Lorenzo Galli <lrnzgll@gmail.com>
2022-10-08 13:37:17 +0100

    add background image

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'background': "url('background.jpg')",
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
